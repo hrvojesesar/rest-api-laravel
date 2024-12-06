da975b41915efc26879d76eaf9c00004fbd62fb6
hrvojesesar <hrvojesesar28@gmail.com>
2024-12-06 13:17:49 +0100

    Created Login API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -46,5 +47,40 @@ class UserController extends Controller
             'message' => 'User registered successfully',
             'data' => $user
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found, please register!'
+            ], 404);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid password!'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User logged in successfully',
+            'data' => [
+                'user' => $user,
+                'token' => $token
+            ]
+        ]);
     }
 }
