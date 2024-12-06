7816d77736c34b37b2b7d5e6943915a47aab3053
hrvojesesar <hrvojesesar28@gmail.com>
2024-12-06 15:44:12 +0100

    Created Roles and Permissions

---
+++ app/Models/Permission.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    protected $primaryKey = 'id';
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permission', 'permission_id', 'role_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_permission', 'permission_id', 'user_id');
+    }
+}
